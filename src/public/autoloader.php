c1fbc119d893993d9085f6b28a04a4d0617a3073
Arpit jayswal <arpitjayswal210@gmail.com>
2025-12-25 22:14:44 +0530

    Local changes before pull

--- src/public/autoloader.php
+++ src/public/autoloader.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 spl_autoload_register(function($class_path){
     echo __DIR__."<br>autoloader run<br>";
     echo $class_path."<br>";
-    $path=__DIR__. "/../".str_replace("\\", "/", $class_path).".php";
+    $path=__DIR__. "/..". DIRECTORY_SEPARATOR.str_replace("\\", "/", $class_path).".php";
     var_dump($path);
 
     if(file_exists($path)){
